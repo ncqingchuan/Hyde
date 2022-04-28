871df624fe93dc482c4a760e1aa600ff4ab95516
ncqingchuan <ncqingchuan@126.com>
2022-04-28 22:57:26 +0800

    Update App.scala
    ceshi

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -2,12 +2,11 @@ import kafkautili.Producer
 import scala.collection.mutable
 import org.apache.kafka.clients.producer.RecordMetadata
 import kafkautili.Consumer
-
 object App {
   def main(args: Array[String]): Unit = {
+    println("hello world")
     val server = "bigdata:9092"
     val topic = "kafka-action"
-
     Consumer(server, "test").RecieveMessage(
       topic,
       partition = null,
@@ -15,6 +14,5 @@ object App {
         println(t.value())
       }
     )
-
   }
 }
