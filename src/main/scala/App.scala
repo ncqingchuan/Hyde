01193b7308cebebb629a43c032c9d29b17087844
ncqingchuan <ncqingchuan@126.com>
2022-05-06 22:47:02 +0800

    ert

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -4,7 +4,7 @@ import org.apache.kafka.clients.producer.RecordMetadata
 import kafkautili.Consumer
 object App {
   def main(args: Array[String]): Unit = {
-    println("hello world")
+    // println("hello world")
     val server = "bigdata:9092"
     val topic = "kafka-action"
     Consumer(server, "test").RecieveMessage(
